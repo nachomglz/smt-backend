7fb3bb7374d151dff278f46eef629f0fb39e4995
nachomglz <nachopad3@gmail.com>
2023-03-01 00:04:12 +0100

    updated main

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     std::env::var("MONGODB_ROOT_PASSWORD").expect("[DB] ~ Password variable hasn't been provided");
 
     smt_backend::run_api().await?;
+
+    println!("[INFO] The API is running 🚀🚀🚀!");
     Ok(())
 }
