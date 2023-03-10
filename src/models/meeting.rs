22174b1efc3804b942d4f55b46f4a43bb4cb8fd6
nachomglz <nachopad3@gmail.com>
2023-03-10 21:11:39 +0100

    Added CRUD Create endpoint for meetings

--- src/models/meeting.rs
+++ src/models/meeting.rs
@@ -1,7 +1,7 @@
 use crate::utils;
 use chrono::{DateTime, Utc};
 use mongodb::bson::oid::ObjectId;
-use rocket::{self, http::Status, State};
+use rocket::{self, http::Status, serde::json::Json, State};
 use serde::{Deserialize, Serialize};
 
 use crate::{config::Pool, utils::responders::Response};
@@ -12,12 +12,31 @@ pub struct Meeting {
     id: Option<ObjectId>,
     /// Real duration of the meeting
     duration: u16,
+    /// Date and time when the meeting started
     #[serde(with = "bson::serde_helpers::chrono_datetime_as_bson_datetime")]
     date_utc: DateTime<Utc>,
 }
 
-#[rocket::post("/new")]
-pub async fn new(db_pool: &State<Pool>) -> Result<Response<Meeting>, Status> {
+#[rocket::post("/new", format = "json", data = "<meeting>")]
+pub async fn new(
+    db_pool: &State<Pool>,
+    meeting: Json<Meeting>,
+) -> Result<Response<Meeting>, Status> {
     let collection = utils::db::get_collection::<Meeting>(db_pool, "meetings").await;
-    todo!()
+
+    let mut new_meeting = meeting.0.clone();
+
+    let result = collection.insert_one(&new_meeting, None).await;
+
+    match result {
+        Ok(result) => {
+            let id = result.inserted_id;
+            new_meeting.id = Some(id.as_object_id().unwrap());
+            Ok(Response::Created(Json(new_meeting)))
+        }
+        Err(error) => {
+            eprintln!("[INSERT][MEETING] ~ {}", error);
+            Err(Status::InternalServerError)
+        }
+    }
 }
